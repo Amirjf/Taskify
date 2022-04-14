86bbdc32fc48c85496a3af3ac2c795037e1cbc01
AmirMasoud Jafari <amirmasoodjafari1677@gmail.com>
2022-04-14 12:54:45 +0430

    fix loading global css

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import { BrowserRouter } from "react-router-dom";
 import { TasksProvider } from "./context/TasksContext";
 import { UserProvider } from "./context/UserContext";
 
-import "react-toastify/dist/ReactToastify.min.css";
-import "./assets/styles/_global.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/styles/_global.scss";
+import "react-toastify/dist/ReactToastify.min.css";
 import "css.gg/icons/all.css";
 import "boxicons/css/boxicons.min.css";
 
