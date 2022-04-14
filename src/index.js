7f0fef2c52609f8af028e73b267027a1442b5b29
AmirMasoud Jafari <amirmasoodjafari1677@gmail.com>
2022-04-14 12:32:13 +0430

    added redirect

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./assets/styles/_global.scss";
-import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
 import { ThemeProvider } from "./context/ThemeContext";
 import { FullScreenProvider } from "./context/FullScreenContext";
-import "css.gg/icons/all.css";
-import "boxicons/css/boxicons.min.css";
+
 import { BrowserRouter } from "react-router-dom";
-import "react-toastify/dist/ReactToastify.min.css";
-import App from "./App";
 import { TasksProvider } from "./context/TasksContext";
 import { UserProvider } from "./context/UserContext";
+
+import "react-toastify/dist/ReactToastify.min.css";
+import "./assets/styles/_global.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "css.gg/icons/all.css";
+import "boxicons/css/boxicons.min.css";
 
 ReactDOM.render(
   <BrowserRouter>
